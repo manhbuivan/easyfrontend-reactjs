06dd1e0b16b882060f2315740095c324d723d0af
Van Manh <mbv.manhbuivan@gmail.com>
2021-05-13 23:46:16 +0700

    new update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import TodoFeature from './features';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Van Manh dep trai
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TodoFeature/>
     </div>
   );
 }
---
+++ src/features/index.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import TodoList from './components/TodoList';
+
+TodoFeature.propTypes = {
+    
+};
+
+function TodoFeature(props) {
+    const listPlay = [
+        {
+            id : 1,
+            title : "an"
+        },
+        {
+            id : 2,
+            title : "choi"
+        },
+        {
+            id : 3,
+            title : "ngu"
+        },
+        {
+            id : 14,
+            title : "code"
+        },
+    ]
+    return (
+        <div>
+            <h2>Manh dep trai</h2>
+            <TodoList todoList = {listPlay}/>
+        </div>
+    );
+}
+
+export default TodoFeature;
