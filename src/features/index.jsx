507b9b837c32781e0954163c75c9b7a2b331e73e
Van Manh <mbv.manhbuivan@gmail.com>
2021-05-14 15:49:33 +0700

    updateee

--- src/features/index.jsx
+++ src/features/index.jsx
@@ -28,6 +28,7 @@ function TodoFeature(props) {
     return (
         <div>
             <h2>Manh dep trai</h2>
+            <h1>bui van manh</h1>
             <TodoList todoList = {listPlay}/>
         </div>
     );
